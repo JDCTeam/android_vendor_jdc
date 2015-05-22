25cc42d7440ed7f6e9de6e77d78e53cbbc2bbeea
Mattia D'Alleva <antaresone@antaresone.eu>
2015-05-22 03:48:15 +0200

    common: ship STweaks
    Thanks to @gokhanmoral for base code and to @B--B for Material UI and general updates.
    
    Base source: https://github.com/gokhanmoral/STweaks
    New source: https://github.com/B--B/STweaks
    
    No ifeq directive here in this branch, I'm going to ship it by default.

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,8 @@
 # EXT4/F2FS format script
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/bin/format.sh:install/bin/format.sh
+    
+# STweaks
+PRODUCT_COPY_FILES += \
+    vendor/jdc/prebuilt/common/app/stweaks/stweaks.apk:system/app/STweaks/STweaks.apk
+
