de49d3ac3c56aaadd3be465721e773e7c571f3af
Mattia D'Alleva <antaresone@antaresone.eu>
2015-05-23 01:17:30 +0200

    vendor: general update
    Ready for JDCTeam GitHub.
    
    - Added ifeq directive for custom format script
    - Added infos about custom format script and STweaks + how-to
    - Added STweaks license
    - General update: Jflte Developers Connection Team to JDCTeam

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,8 @@
 # EXT4/F2FS format script
+ifeq ($(TARGET_CUSTOM_SYSTEM_FORMAT),true)
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/bin/format.sh:install/bin/format.sh
+endif
     
 # STweaks
 ifeq ($(TARGET_SHIP_STWEAKS),true)
