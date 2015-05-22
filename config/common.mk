9b94b892014472931b78cd88f6b4d3bbef06bc10
Mattia D'Alleva <antaresone@antaresone.eu>
2015-05-22 03:48:15 +0200

    common: ship STweaks
    Thanks to @gokhanmoral for base code and to @B--B for Material UI and general updates.
    
    Base source: https://github.com/gokhanmoral/STweaks
    New source: https://github.com/B--B/STweaks

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,9 @@
 # EXT4/F2FS format script
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/bin/format.sh:install/bin/format.sh
+    
+# STweaks
+ifeq ($(TARGET_SHIP_STWEAKS),true)
+PRODUCT_COPY_FILES += \
+    vendor/jdc/prebuilt/common/app/stweaks/stweaks.apk:system/app/STweaks/STweaks.apk
+endif
