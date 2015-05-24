02b4624f0182da110ed477b5c07b19e2e1e524eb
Kryten2k35 <Kryten2k35@gmail.com>
2015-05-24 20:53:15 +0100

    Added OTA properties
    Change-Id: Ie708c11def6cd1663b9b425b04721e16863d5e96

--- config/common.mk
+++ config/common.mk
@@ -9,3 +9,8 @@ ifeq ($(TARGET_SHIP_STWEAKS),true)
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/app/stweaks/stweaks.apk:system/app/STweaks/STweaks.apk
 endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.ota.romname=Optimized-CM-12.1 \
+	ro.ota.version=$(shell date -u +%Y%m%d) \
+	ro.ota.manifest=https://romhut.com/roms/Optimized-CM-121/ota.xml
