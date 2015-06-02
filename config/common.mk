66433b5e39b4a37f85cc14623fb061e34580b3fa
Mattia D'Alleva <antaresone@antaresone.eu>
2015-06-02 20:13:15 +0200

    Makefile: temporary disable ifeq directives
    Aren't found in BoardConfig...

--- config/common.mk
+++ config/common.mk
@@ -1,14 +1,14 @@
 # EXT4/F2FS format script
-ifeq ($(TARGET_CUSTOM_SYSTEM_FORMAT),true)
+# ifeq ($(TARGET_CUSTOM_SYSTEM_FORMAT),true)
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/bin/format.sh:install/bin/format.sh
-endif
+# endif
     
 # STweaks
-ifeq ($(TARGET_SHIP_STWEAKS),true)
+# ifeq ($(TARGET_SHIP_STWEAKS),true)
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/app/stweaks/stweaks.apk:system/app/STweaks/STweaks.apk
-endif
+# endif
 
 # OTA Updates
 PRODUCT_PROPERTY_OVERRIDES += \
