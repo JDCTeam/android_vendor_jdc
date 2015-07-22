cea40fe6d9b24ad913763f6559f7a33ad6ec3bed
Mattia D'Alleva <antaresone@antaresone.eu>
2015-07-22 22:20:34 +0200

    Clean

--- README.md
+++ README.md
@@ -4,29 +4,3 @@ Copyright 2015 - JDCTeam
 Addons/common files for ROMs
 
 WORK IN PROGRESS.
-
-#### F2FS format script:
-
-Smart way to handle both ext4/F2FS at ROM's installation. If TARGET_USERIMAGES_USE_F2FS is set to true, edify_generator from android_build will set format and mount commands for F2FS (in case this file system is enabled for /system too). This isn't a safe way because ROM's installation will be denied on non-F2FS compatible recovery; instead, in this case format as ext4 with mke2fs. Also let the user choose what file system he wants (requires a F2FS-compatible recovery).
-
-Needs:
-
-- **TARGET_CUSTOM_SYSTEM_FORMAT := true** in your BoardConfig
-
-- https://github.com/AntaresOne/android_build/commit/e9b6c07db536e28c9b8d05fb0fc0984149983d2a in your local source
-
-In your source's root:
-
-	cd build && git fetch https://github.com/AntaresOne/android_build cm-12.1-custom && git cherry-pick e9b6c07db536e28c9b8d05fb0fc0984149983d2a && cd ..
-
-You're now ready to build
-
-#### STweaks:
-
-Copyright 2012 - Gokhan Moral
-
-Kernel settings configuration app with dynamic UI generation, made by gokhanmoral. Android Lollipop's Material UI and some additions are made by B--B. We use it with Alucard kernel.
-
-Needs:
-
-- **TARGET_SHIP_STWEAKS := true** in your BoardConfig
--- config/common.mk
+++ config/common.mk
@@ -1,12 +1,14 @@
+# Changelog
+PRODUCT_COPY_FILES += \
+    vendor/jdc/Changelog.md:system/etc/Changelog.md
+
+# Custom format script
+PRODUCT_COPY_FILES += \
+    vendor/jdc/prebuilt/common/bin/format.sh:install/bin/format.sh
+
 # Dancing LED
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/etc/init.d/01led:system/etc/init.d/01led
-
-# EXT4/F2FS format script
-# ifeq ($(TARGET_CUSTOM_SYSTEM_FORMAT),true)
-PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/bin/format.sh:install/bin/format.sh
-# endif
 
 # For keyboard gesture typing
 PRODUCT_COPY_FILES += \
@@ -15,23 +17,15 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/jdc/overlay/common
 
-# STweaks
-# ifeq ($(TARGET_SHIP_STWEAKS),true)
-PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/app/stweaks/stweaks.apk:system/app/STweaks/STweaks.apk
-# endif
-
-# OTA Updates
+# Packages
+PRODUCT_PACKAGES += \
+    STweaks \
+    OTAUpdates
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.ota.romname=Optimized-CM-12.1 \
-	ro.ota.version=$(shell date -u +%Y%m%d) \
-	ro.ota.manifest=https://romhut.com/roms/Optimized-CM-121/ota.xml
+    ro.ota.romname=Optimized-CM-12.1 \
+    ro.ota.version=$(shell date -u +%Y%m%d) \
+    ro.ota.manifest=https://romhut.com/roms/Optimized-CM-121/ota.xml
 
 # Take a logcat
-
 PRODUCT_COPY_FILES += \
     vendor/jdc/prebuilt/common/bin/take_log:system/bin/take_log
-
-# Changelog
-PRODUCT_COPY_FILES += \
-    vendor/jdc/Changelog.md:system/etc/Changelog.md
