d7062ec9f9311353d52c5db8af0d361b8286a07a
Josh Chasky <jchasky@gmail.com>
2016-09-08 19:39:41 -0400

    Allow AOSP KB to gesture type
    - Add condition for 32/64 devices
    
    Change-Id: Ia19043df237f41ebe0ff556fe794eb115d6ec212

--- config/common.mk
+++ config/common.mk
@@ -131,6 +131,17 @@ PRODUCT_PACKAGES += \
     libffmpeg_omx \
     media_codecs_ffmpeg.xml
 
+# LatinIME gesture typing
+ifeq ($(TARGET_ARCH),arm64)
+PRODUCT_COPY_FILES += \
+    vendor/gzosp/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/gzosp/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/gzosp/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/gzosp/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+endif
+
 PRODUCT_PROPERTY_OVERRIDES += \
     media.sf.omx-plugin=libffmpeg_omx.so \
     media.sf.extractor-plugin=libffmpeg_extractor.so
