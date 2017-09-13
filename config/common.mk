c897f0652a30436a18a8cc38a25c287545419d5d
Josh Chasky <jchasky@gmail.com>
2017-09-13 11:11:52 +0200

    Fix AOSP gesture typing

--- config/common.mk
+++ config/common.mk
@@ -39,6 +39,17 @@ else
     ro.device.cache_dir=/cache
 endif
 
+# LatineIME Gesture swyping
+ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/liquid/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/liquid/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+endif
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
