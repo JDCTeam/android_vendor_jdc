67fbbf6a7bb4c7ebe81bef2a522b32046948b1dc
dkati <dkatikaridis@gmail.com>
2018-07-11 15:00:05 +0300

    Bring up init.jdcteam.rc
    Change-Id: I76d4cd9e2fb5ef8046809c712ed63481e442a238

--- prebuilt/common/bin/proximity.sh
+++
@@ -1,8 +0,0 @@
-#!/sbin/sh
-#
-# Copyright (c) 2016 - JDCTeam
-#
-#!/system/bin/sh
-echo 0 > /sys/class/sensors/proximity_sensor/prox_cal
-echo 1 > /sys/class/sensors/proximity_sensor/prox_cal
-exit 0
