LineageOS/android_vendor_lineage

lineage: Exclude all lineage overlays from RRO
None of this is device specific and therefore needs to be excluded from
RRO overlay packages

Change-Id: I24d882b783a8d7c9e494298e2c26c8ef689a515a

config/common.mk
@@ -240,6 +240,7 @@ PRODUCT_PACKAGES += \
 endif
 endif
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/common
 
 PRODUCT_VERSION_MAJOR = 16

JDCTeam/android_vendor_jdc

config/common.mk
@@ -192,6 +192,7 @@ endif
 $(call inherit-product-if-exists, vendor/liquid/prebuilt/common/prebuilt.mk)
 
 # Vendor Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/liquid/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/liquid/overlay/common
 
 # Version System