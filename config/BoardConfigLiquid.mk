1ea0e25b9e3b21fc164072b5ed49b3fd0f23ce7e
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-18 21:53:24 -0700

    liquid: Dynamically generate kernel headers using liquid generator
    Add a soong vendor plugin for kernel config variables so we can use
    these in go.
    
    Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6

--- config/BoardConfigLiquid.mk
+++ config/BoardConfigLiquid.mk
@@ -4,3 +4,5 @@ include vendor/liquid/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/liquid/config/BoardConfigQcom.mk
 endif
+
+include vendor/liquid/config/BoardConfigSoong.mk
