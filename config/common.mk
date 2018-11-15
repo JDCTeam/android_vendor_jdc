cccea912582e0a097870793c4806142339c2420a
ElDainosor <eldainosor@gmail.com>
2018-11-16 01:48:15 +0530

    Add more QS styles [3/3]

--- config/common.mk
+++ config/common.mk
@@ -87,19 +87,24 @@ PRODUCT_PACKAGES += \
 
 # QS tile styles
 PRODUCT_PACKAGES += \
-    QStileCircleTrim \
     QStileDefault \
-    QStileDualToneCircle \
-    QStileOreo \
-    QStileOreoCircleTrim \
-    QStileOreoSquircleTrim \
-    QStileSquircleTrim \
-    QStileMountain \
+    QStileCircleTrim \
     QStileCircleDualTone \
     QStileCircleGradient \
     QStileDottedCircle \
+    QStileDualToneCircle \
+    QStileInk \
+    QStileInkdrop \
+    QStileMountain \
     QStileNinja \
+    QStileOreo \
+    QStileOreoCircleTrim \
+    QStileOreoSquircleTrim \
     QStilePokesign \
+    QStileSquaremedo \
+    QStileSquircle \
+    QStileSquircleTrim \
+    QStileTeardrop \
     QStileWavey
 
 # QS header styles
