760a976680840a11f47d2c330e1102d143426bf0
Kshitij Gupta <kshitijgm@gmail.com>
2018-12-27 16:29:23 +0530

    vendor: QS themes [3/3]

--- config/common.mk
+++ config/common.mk
@@ -123,6 +123,11 @@ PRODUCT_PACKAGES += \
     QSHeaderAccent \
     QSHeaderTransparent
 
+# QS accents
+PRODUCT_PACKAGES += \
+    QSAccentBlack \
+    QSAccentWhite
+
 # UI themes
 PRODUCT_PACKAGES += \
     AOSPUI \
