Havoc-OS/android_vendor_havoc

vendor: Add GBoard MD2 themes
Thanks to @RadekBledowski for the themes.
Fixed them up a bit according to MD2 guidelines.

config/common.mk
@@ -324,9 +324,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     rsync
 
-# Set Pixel blue light theme on Gboard
+# GBoard Themes
+PRODUCT_COPY_FILES += \
+    vendor/havoc/themes/GBoard/MD2.zip:system/etc/gboard/MD2.zip \
+    vendor/havoc/themes/GBoard/MD2Black.zip:system/etc/gboard/MD2Black.zip \
+    vendor/havoc/themes/GBoard/MD2Dark.zip:system/etc/gboard/MD2Dark.zip
+
+# Set Pixel blue light MD2 theme on Gboard
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.ime.theme_id=5
+    ro.com.google.ime.themes_dir=/system/etc/gboard \
+    ro.com.google.ime.theme_file=MD2.zip
 
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \

JDCTeam/android_vendor_jdc

config/common.mk
@@ -257,6 +257,17 @@ PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
     vendor/liquid/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
+# GBoard Themes
+PRODUCT_COPY_FILES += \
+    vendor/liquid/themes/GBoard/MD2.zip:system/etc/gboard/MD2.zip \
+    vendor/liquid/themes/GBoard/MD2Black.zip:system/etc/gboard/MD2Black.zip \
+    vendor/liquid/themes/GBoard/MD2Dark.zip:system/etc/gboard/MD2Dark.zip
+
+# Set Pixel blue light MD2 theme on Gboard
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.ime.themes_dir=/system/etc/gboard \
+    ro.com.google.ime.theme_file=MD2.zip
+
 # TWRP
 ifeq ($(WITH_TWRP),true)
 include vendor/liquid/config/twrp.mk