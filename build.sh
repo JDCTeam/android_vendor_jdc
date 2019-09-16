55a3a02469fac178a88dfb170f3d5a67ee6e660a
dkati <dkatikaridis@gmail.com>
2019-09-16 17:29:34 +0300

    build.sh: Out file is on croot now

--- build.sh
+++ build.sh
@@ -62,7 +62,8 @@ buildRelease()
     	
 	if [ "$?" == 0 ]; then
 		echo "Build done"
-		mv $OUT/lineage*.zip $OUT/Optimized-LineageOS-$LOS_VER-V-$ROM_VERSION-Signed.zip 
+		croot
+		mv target*.zip Optimized-LineageOS-$LOS_VER-V-$ROM_VERSION-Signed.zip 
 	else
 		echo "Build failed"
 	fi
