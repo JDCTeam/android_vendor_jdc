2e6441ff3c6ad1ba7a86904ac0858b0b85818966
Buff99 <buffbuffster@gmail.com>
2019-09-30 22:38:56 +0100

    prebuilts: remove VIA browser as we have Jelly instead

--- prebuilt/common/apk/Android.mk
+++ prebuilt/common/apk/Android.mk
@@ -14,16 +14,6 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-#ViaBrowser
-include $(CLEAR_VARS)
-LOCAL_MODULE := ViaBrowser
-LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_DEX_PREOPT := false
-include $(BUILD_PREBUILT)
-
 #AEXPapers
 include $(CLEAR_VARS)
 LOCAL_MODULE := AEXPapers
